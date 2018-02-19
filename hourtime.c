37faaab7a6dc39aa8f12d0bd977bd0fef9dba8cd
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-19 15:58:52 +0530

    c pgm
    good

---
+++ hourtime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void maain())
+{
+
+ int a,b,c,d,g,h;
+ clrscr();
+ 
+ printf("enter the time");
+scanf("%d%d\n %d%d",&a,&b,&c,&d);
+
+g=a-c;
+h=b-d;
+{
+printf("%d %d",g,h);
+}
+}
